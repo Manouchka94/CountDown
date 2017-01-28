4eb987a23fa88a60a034a8c04084e440b45f038d
Manouchka94 <emmanuel.drecq@gmail.com>
2017-01-28 20:45:26 +0100

    commit 2

--- CountDown.js
+++ CountDown.js
@@ -4,7 +4,7 @@ function getTimeRemaining(endtime){
 	var minutes = Math.floor( (t/1000/60) % 60 );
 	var hours = Math.floor( (t/(1000*60*60)) % 24 );
 	var days = Math.floor( t/(1000*60*60*24) );
-	
+	 
 	return {
 		'total': t,
 		'minutes': minutes,
